ec3dae2c8f9b32874058f430440bb49c4c57aaf5
igor.souza <igor.souza@premiumtec.com.br>
2022-05-06 12:36:37 -0300

    Feat: Inserção de alguns itens visuais

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export default function Card (){
+
+
+
+
+    return(
+       <div id='post' className='container'>
+           <h1></h1>
+       </div>
+
+    )
+}
--- src/views/Templates/TemplateAdmin/index.jsx
+++ src/views/Templates/TemplateAdmin/index.jsx
@@ -1,5 +1,9 @@
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { BsCalendarEvent, BsNewspaper, BsShop } from 'react-icons/bs';
 import Avatar from "../../../assets/images/img-avatar.png";
 import Logo from "../../../assets/images/logo-atletica.png";
+import Card from '../../../components/Card';
 import "./style.css";
 
 export default function TemplateAdmin() {
@@ -33,13 +37,47 @@ export default function TemplateAdmin() {
       <main id="main" className="container">
       <section id="main-navbar">
           <ul >
-              <li>Eventos</li>
-              <li>Feed</li>
-              <li>Produtos</li>
+              <li>
+                <a href="#">
+                  <BsCalendarEvent/><br />
+                  Eventos
+                  </a>
+              </li>
+              <li>
+                <a href="#">
+                  <BsNewspaper/><br />
+                  Feed
+                  </a>
+              </li>
+              <li>
+                <a href="#">
+                  <BsShop/><br />
+                  Produtos
+                  </a>
+              </li>
           </ul>
       </section>
-        <section id="left"></section>
-        <section id="center"></section>
+        <section id="left">
+          <div id="calendario">
+          <Calendar/>
+
+
+          </div>
+
+        </section>
+        <section id="center">
+
+          <Card/>
+          <Card/>
+
+          <Card/>
+
+          <Card/>
+
+          <Card/>
+
+
+        </section>
         <section id="right"></section>
       </main>
     </>
